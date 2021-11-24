68b5c4cf462acda74524654b84972b7bcbb94dca
luis <luis@gmail.com>
2021-11-23 22:27:02 -0500

    components added

--- taxiBooking/src/app/app.module.ts
+++ taxiBooking/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    CardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
